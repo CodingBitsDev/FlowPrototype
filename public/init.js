27b9abd2ed4af1178d27755de2118a2492d66a7d
Nils Rohr <nils.rohr@zreality.com>
2021-03-20 00:19:34 +0100

    added first finder test

--- public/init.js
+++ public/init.js
@@ -1,13 +1,26 @@
 (function(){
     //alert("test")
+    let clickedElements = [];
     document.body.addEventListener('click',(e) => {
         if (e && e.path){
             let path = e.path;
             let smallestClicked = path[0];
-            console.log(smallestClicked)
-            smallestClicked.style.borderColor = "red"
-            smallestClicked.style.borderWidth = "2px"
-            smallestClicked.style.borderStyle = "solid"
+            console.log(smallestClicked);
+            console.log(smallestClicked.outerHTML);
+
+            let elem = findElementByString(smallestClicked)
+            elem.style.borderColor = "red"
+            elem.style.borderWidth = "2px"
+            elem.style.borderStyle = "solid"
+            //save clicked
+            clickedElements.push(smallestClicked);
         }
     }, true); 
+    
 })();
+
+function findElementByString(string){
+    $("." + test.match(/class="([^"]*)"/gm)[0].replace("class=", "").replaceAll("\"", "").replaceAll(" ", ".")).toArray().find( e => {
+        return (e.outerHTML == string);
+    })
+}
