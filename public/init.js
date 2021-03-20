f220519483ee69ce7ef743364a98590073ca8b93
Nils Rohr <nils.rohr@zreality.com>
2021-03-20 03:08:17 +0100

    element detection working

--- public/init.js
+++ public/init.js
@@ -1,26 +1,36 @@
 (function(){
-    //alert("test")
-    let clickedElements = [];
     document.body.addEventListener('click',(e) => {
         if (e && e.path){
             let path = e.path;
             let smallestClicked = path[0];
-            console.log(smallestClicked);
-            console.log(smallestClicked.outerHTML);
 
-            let elem = findElementByString(smallestClicked)
-            elem.style.borderColor = "red"
-            elem.style.borderWidth = "2px"
-            elem.style.borderStyle = "solid"
-            //save clicked
-            clickedElements.push(smallestClicked);
+            let elem = findElementByString(smallestClicked.outerHTML)
+            if (elem){
+                elem.style.borderColor = "red"
+                elem.style.borderWidth = "2px"
+                elem.style.borderStyle = "solid"
+            }
         }
     }, true); 
     
 })();
 
 function findElementByString(string){
-    $("." + test.match(/class="([^"]*)"/gm)[0].replace("class=", "").replaceAll("\"", "").replaceAll(" ", ".")).toArray().find( e => {
-        return (e.outerHTML == string);
-    })
+    let stringMatch = string.match(/class="([^"]*)"/gm);
+    if (!stringMatch){
+        let elementList =  document.getElementsByTagName("*");
+        for (let i = 0; i < elementList.length; i++){
+            if (elementList[i].outerHTML == string) return elementList[i];
+        }
+        return null;
+    } 
+    let classString = stringMatch[0];
+    if (classString) {
+        let classQuery = classString.replace("class=", "").replaceAll("\"", "").replaceAll(" ", ".");
+        let result = $("." + classQuery).toArray().find( e => {
+            return (e.outerHTML == string);
+        })
+        return result
+    }
+    return null
 }
