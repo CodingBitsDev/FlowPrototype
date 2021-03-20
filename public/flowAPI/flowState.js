f08a48d4469a9f58e80dc7a194d40f6eb79dcdc5
Nils Rohr <nils.rohr@zreality.com>
2021-03-20 21:36:53 +0100

    created state and react floating button

---
+++ public/flowAPI/flowState.js
@@ -0,0 +1,87 @@
+let flowState = {
+    mainState: 0,
+    currenStep: 0,  
+    clickData: [],
+}
+
+let stateListeners = new Map()
+
+let queueActive = true;
+let stateQueue = [];
+
+function _initState(){
+   //Laoad state from storage with key of page 
+}
+
+function _saveState(newState){
+    flowState = newState;
+   //Save state to storage with key of page
+}
+
+function _updateState(){
+    queueActive = true;
+    while (stateQueue.length > 0){
+        let stateData = stateQueue.unshift();
+        //Notify State Listener 
+        let traverseKeys = (stateData, keyPath = "") => {
+            Object.keys(stateData).forEach( key => {
+                let currentKeyPath = keyPath + !keyPath ? "" : "." + key
+                let value = stateData[key]
+                stateListeners.get(currentKeyPath).forEach( (listener) => {
+                    listener.cb(flowState, stateData);
+                })
+                if (typeof value === 'object' && value !== null){
+                    traverseKeys(value, currentKeyPath);
+                }
+            });
+        }
+        traverseKeys(stateData);
+        //Update to new state
+        _saveState({...flowState, ...stateData});
+    }
+    queueActive = false;
+}
+
+function setState( stateData ){
+    stateQueue.push(stateData);
+    if( !queueActive ) _updateState();
+}
+
+function addStateListener( stateVarString, cb, id = "" ){
+    let currentListeners = stateListeners.get(stateVarString) || [];
+    currentListeners.push({ cb, id})
+    stateListeners.set(currentListeners);
+}
+
+function removeListener( stateVarString, cb, id  ){
+    let currentListeners = stateListeners.get(stateVarString) || [];
+    currentListeners = currentListeners.filter( (listener) => {
+        if (id && listener.id == id ){
+            return false;
+        } 
+        if (listener.cb == cb ){
+            return false;
+        } 
+        return true;
+    })
+    stateListeners.set(currentListeners);
+}
+
+function getState(stateString = ""){
+    let stateArray = stateString.split(".");
+    let result = null;
+    for (let index = 0; index < stateArray.length; index++) {
+        const stateKey = stateArray[index];
+        if (flowState[stateKey]) return null;
+        result = flowState[stateKey]
+    }
+    return result;
+}
+
+(function(){
+    _initState();
+    window.flowAPI.setState = setState;
+    window.flowAPI.addStateListener = addStateListener;
+    window.flowAPI.removeListener = removeListener;
+    window.flowAPI.getState = getState;
+})()
--- public/init.js
+++ public/init.js
@@ -1,4 +1,26 @@
 (function(){
+    setUpFirebase();
+    window.flowAPI = {};
+    window.flowAPI.startTracking = startTracking;
+    window.flowAPI.findElementByString = findElementByString;
+})();
+
+function setUpFirebase(){
+    // Your web app's Firebase configuration
+    let firebaseConfig = {
+        apiKey: "AIzaSyASWzyRjBuQ97djqgpmm0toF7wRxSeGgT0",
+        authDomain: "flow-609c0.firebaseapp.com",
+        projectId: "flow-609c0",
+        storageBucket: "flow-609c0.appspot.com",
+        messagingSenderId: "779121560180",
+        appId: "1:779121560180:web:1da31e87127095ffc49f21"
+    };
+    // Initialize Firebase
+    // console.log("###", firebase.initializeApp(firebaseConfig));
+    // window.flowAPI.firebase = firebase;
+}
+
+function startTracking(){
     document.body.addEventListener('click',(e) => {
         console.log
         let clickedElement = null
@@ -14,8 +36,7 @@
             }
         }
     }, true); 
-    
-})();
+}
 
 function findElementByString(string){
     let stringMatch = string.match(/class="([^"]*)"/gm);
